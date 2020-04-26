9d306aa897fb6576256b30a96d6df9360f71ea15
eloc0n <johnisb.21@gmail.com>
2020-04-26 14:47:54 +0300

    created model for orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from decimal import Decimal
+
+from django.contrib.auth.models import User
+from django.db import models
+from accounts.models import UserAddres
+from cart.models import Cart
 
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('Started', 'Started'),
+    ('Abandoned', 'Abandoned'),
+    ('Finished', 'Finished'),
+)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    order_id = models.CharField(max_length=120, unique=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(max_length=120, choices=STATUS_CHOICES, default='Started')
+    
+    address = models.ForeignKey(UserAddres, on_delete=models.SET_NULL, blank=True, null=True)
+    
+    sub_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
+    tax_total = models.DecimalField(default=0.00, max_digits=1000, decimal_places=2)
+    final_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
+    
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.order_id
+
+
+    def get_final_tax_amount(self):
+        instance = Order.objects.get(id=self.id)
+        two_decimals = Decimal(10) ** -2
+        instance.tax_total = Decimal(Decimal('0.24') * Decimal(self.sub_total)).quantize(two_decimals)
+        instance.save()
+
+        return  instance.tax_total
+
+    def get_final_amount(self):
+        instance = Order.objects.get(id=self.id)
+        instance.final_total = Decimal(self.sub_total) + Decimal(instance.tax_total)
+        instance.save()
+
+        return  instance.final_total
--- orders/views.py
+++ orders/views.py
@@ -5,18 +5,32 @@ from django.conf import settings
 from accounts.forms import UserAddressForm
 from accounts.models import UserAddres
 # from .models import Order
-# from shopping_cart.models import Cart
+# from cart.models import Cart
 # from .utils import id_generator
 
 
 # Create your views here.
 @login_required # require user login
 def address(request):
-    # assign an address 
-    address_form = UserAddressForm(request.POST or None) 
+    try:
+        user_address = UserAddres.objects.filter(user=request.user).last() # grab stored address
+        initial = {
+                    'address': user_address.address,
+                    'address2': user_address.address2,
+                    'city': user_address.city,
+                    'country': user_address.country,
+                    'zipcode': user_address.zipcode,
+                    'phone': user_address.phone
+        }
+        
+        address_form = UserAddressForm(request.POST or initial) # display stored address
+
+    except:
+        address_form = UserAddressForm(request.POST or None) # assign new address
+        
     if request.method == 'POST':
         # try:
-        #     user_address = UserAddres.objects.get(user=request.user)
+        #     user_address = UserAddres.objects.filter(user=request.user).first()
         #     user_address.delete()
         # except:
         #     user_address = None
@@ -25,7 +39,7 @@ def address(request):
             new_address = address_form.save(commit=False)
             new_address.user = request.user
             new_address.save()
-            return redirect(reverse('checkout'))
+            return redirect(reverse('home'))
 
     context = {
         'address_form':address_form,
