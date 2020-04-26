ad5889a079ce9dbc3139fc80ca43ba4ac38ce4a8
eloc0n <johnisb.21@gmail.com>
2020-04-26 15:03:50 +0300

    connect signal with account app

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):            # connects signals with the app
+        import accounts.signals 
