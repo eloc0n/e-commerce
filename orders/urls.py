cd2e450221091f505380de4dc6b9f7fd080613ae
eloc0n <johnisb.21@gmail.com>
2020-04-27 14:27:22 +0300

    added functionalities in orders/views and stripe charge

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('checkout/', views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
     path('address/', views.address, name='address'),
     # path('orders/', views.orders, name='user_orders'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,23 @@
+import stripe
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from accounts.forms import UserAddressForm
 from accounts.models import UserAddres
-# from .models import Order
-# from cart.models import Cart
-# from .utils import id_generator
+from .models import Order
+from cart.models import Cart
+from .utils import id_generator
+
+try:
+    stripe_pub = settings.STRIPE_PUBLISHABLE_KEY
+    stripe_secret = settings.STRIPE_SECRET_KEY
+except Exception as e:
+    raise NotImplementedError(str(e))
+
+stripe.api_key = stripe_secret
+
 
 
 # Create your views here.
@@ -39,7 +50,7 @@ def address(request):
             new_address = address_form.save(commit=False)
             new_address.user = request.user
             new_address.save()
-            return redirect(reverse('home'))
+            return redirect(reverse('checkout'))
 
     context = {
         'address_form':address_form,
@@ -47,3 +58,84 @@ def address(request):
 
     return render(request, 'orders/address.html', context)
     
+
+
+@login_required # require user login
+def checkout(request):
+    try:
+        the_id = request.session['cart_id'] # if cart id exist grab it
+        cart = Cart.objects.get(id=the_id)
+    except:
+        the_id = None
+        return redirect(reverse('cart'))
+    
+    user_address = UserAddres.objects.filter(user=request.user).last() # Not secure, user can redirect to checkout through url without
+                                                                        # saving/creating address and the latest address 
+                                                                        # in queryset gets assigned instead (might be from previous users)
+   
+    try:
+        new_order = Order.objects.get(cart=cart, address=user_address)
+    except Order.DoesNotExist:
+        new_order = Order()
+        new_order.cart = cart
+        new_order.address = user_address
+        new_order.user = request.user   # assign user to the order
+        new_order.order_id = id_generator() 
+        new_order.save()
+    except:
+        new_order = None
+        # error message
+        return redirect(reverse('cart'))
+    
+    final_amount = 0
+    if new_order is not None: 
+        new_order.sub_total = cart.total    # update sub_total when user leaves checkout to add more products
+        new_order.save()   
+        new_order.get_final_tax_amount()    # final amount
+        final_amount = new_order.get_final_amount()  # final amount with taxes included                                                   
+
+    
+        
+
+    # run credit card
+    if request.method == 'POST':
+        try:
+            user_stripe = request.user.userstripe.stripe_id
+            customer = stripe.Customer.retrieve(user_stripe)
+        except:
+            customer = None
+
+        if customer is not None:
+            token = request.POST['stripeToken']
+            card = stripe.Customer.create_source(customer.id, source=token)
+            card.address_city = user_address.city
+            card.address_line1 = user_address.address
+            card.address_line2 = user_address.address2
+            card.address_country = user_address.country
+            card.address_zip = user_address.zipcode
+            card.save()
+           
+            charge = stripe.Charge.create(
+                amount= int(final_amount * 100),
+                currency="eur",
+                source=card,
+                customer=customer.id,
+                description="My First Test Charge %s" %(request.user.username),
+            )
+
+            if charge['captured']:
+                new_order.status = 'Finished'
+                new_order.save()
+                del request.session['cart_id']
+                del request.session['items_total']
+                # order success message
+        
+                return redirect(reverse('home'))
+    
+
+    context = {
+        'order':new_order,
+        'stripe_pub':stripe_pub,
+    }
+
+    return render(request, 'orders/checkout.html', context)
